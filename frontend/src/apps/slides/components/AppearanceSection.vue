c73ede39e294fb92d096abc0289838912a1b7f03
Gursheen Anand <gursheen@frappe.io>
2026-07-01 15:21:48 +0530

    feat: add appearance section to properties panel

---
+++ frontend/src/apps/slides/components/AppearanceSection.vue
@@ -0,0 +1,45 @@
+<template>
+	<Section label="Appearance" class="border-b py-4">
+		<NumberControl
+			v-if="activeElement.type == 'text'"
+			:modelValue="parseFloat(editorStyles.opacity)"
+			label="Opacity"
+			suffix="%"
+			:min="0"
+			:max="100"
+			:max-digits="3"
+			:step="1"
+			@update:modelValue="(value) => updateProperty('opacity', parseFloat(value))"
+		/>
+		<NumberControl
+			v-else
+			:modelValue="activeElement.opacity"
+			label="Opacity"
+			suffix="%"
+			:min="0"
+			:max="100"
+			:max-digits="3"
+			:step="1"
+			@update:modelValue="(value) => (activeElement.opacity = value)"
+			@change-start="onOpacityStart"
+			@change-end="onOpacityEnd"
+		/>
+	</Section>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+
+import NumberControl from '@/apps/slides/components/controls/NumberControl.vue'
+import Section from '@/apps/slides/components/Section.vue'
+
+import { useTextEditor } from '@/apps/slides/composables/useTextEditor'
+
+import { activeElement } from '@/apps/slides/stores/element'
+
+const setPropertyDeferred = inject('setPropertyDeferred')
+
+const { editorStyles, updateProperty } = useTextEditor()
+
+const { onStart: onOpacityStart, onEnd: onOpacityEnd } = setPropertyDeferred('element', 'opacity')
+</script>
--- frontend/src/apps/slides/components/PropertiesPanel2.vue
+++ frontend/src/apps/slides/components/PropertiesPanel2.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="flex h-full w-72 flex-col overflow-y-auto border-l bg-surface-base px-4">
 		<div v-if="activeElementIds.length">
-			<PositionProperties />
-			<LayoutProperties />
+			<PositionSection />
+			<LayoutSection />
+			<AppearanceSection />
 		</div>
 	</div>
 </template>
@@ -10,13 +11,16 @@
 <script setup>
 import { provide } from 'vue'
 
+import { useDeferredCommit } from '@/apps/slides/composables/useDeferredCommit'
+
 import { activeElement, activeElementIds } from '@/apps/slides/stores/element'
 import { currentSlide } from '@/apps/slides/stores/slide'
 import { commandHistory } from '@/apps/slides/stores/historyMeta'
 import { editElementCommand } from '@/apps/slides/stores/commands'
 
-import PositionProperties from './PositionProperties.vue'
-import LayoutProperties from './LayoutProperties.vue'
+import PositionSection from './PositionSection.vue'
+import LayoutSection from './LayoutSection.vue'
+import AppearanceSection from './AppearanceSection.vue'
 
 const setProperty = (property, value) => {
 	const oldValue = activeElement.value[property]
@@ -31,5 +35,22 @@ const setProperty = (property, value) => {
 	)
 }
 
+const setPropertyDeferred = (level, property) => {
+	if (level === 'element') {
+		return useDeferredCommit(
+			() => activeElement.value?.[property],
+			(oldValue, newValue) =>
+				editElementCommand({
+					slideId: currentSlide.value?.clientId,
+					elementIds: activeElementIds.value,
+					property,
+					oldValue,
+					newValue,
+				}),
+		)
+	}
+}
+
 provide('setProperty', setProperty)
+provide('setPropertyDeferred', setPropertyDeferred)
 </script>
