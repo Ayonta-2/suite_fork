384fa0aa7f7f8898310131423862f67528b61cb1
Akash Tom <me@akashtom.com>
2026-07-29 16:06:57 +0530

    Merge pull request #384 from krantheman/fix/mail-subject-label-focus
    fix(mail): focus subject input when its label is clicked

--- frontend/src/apps/mail/components/ComposeMailEditor.vue
+++ frontend/src/apps/mail/components/ComposeMailEditor.vue
@@ -107,13 +107,16 @@
 						</template>
 					</div>
 				</div>
-				<div v-if="!mailDetails?.type || isMobile" class="flex items-center gap-2">
+				<label
+					v-if="!mailDetails?.type || isMobile"
+					class="flex cursor-text items-center gap-2"
+				>
 					<span class="text-ink-gray-4 text-sm">{{ __('Subject') }}</span>
 					<input
 						v-model="mail.subject"
-						class="flex-1 border-none bg-inherit text-base focus-visible:!ring-0"
+						class="flex-1 cursor-text border-none bg-inherit text-base focus-visible:!ring-0"
 					/>
-				</div>
+				</label>
 			</div>
 		</template>
 		<template #editor="{ editor }">
