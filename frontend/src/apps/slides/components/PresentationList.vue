83c838c6d7758f2e23cabaef18f0ad13e5270678
Gursheen Anand <gursheen@frappe.io>
2026-08-18 05:07:14 +0530

    fix: 10px radius on all thumbnails

--- frontend/src/apps/slides/components/PresentationList.vue
+++ frontend/src/apps/slides/components/PresentationList.vue
@@ -26,7 +26,7 @@
 					<div class="flex flex-col gap-3">
 						<!-- Presentation Card -->
 						<div
-							class="aspect-[16/9] cursor-pointer overflow-hidden rounded-lg shadow-sm dark:border"
+							class="aspect-[16/9] cursor-pointer overflow-hidden rounded-md shadow-sm dark:border"
 							@click="$emit('navigate', presentation.name)"
 						>
 							<div
