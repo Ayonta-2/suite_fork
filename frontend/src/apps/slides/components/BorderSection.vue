f4a4c808777e08cfd24bbd49e05eb70f170061a7
Gursheen Anand <gursheen@frappe.io>
2026-09-16 14:37:33 +0530

    refactor: border writes through editProperty

--- frontend/src/apps/slides/components/BorderSection.vue
+++ frontend/src/apps/slides/components/BorderSection.vue
@@ -10,9 +10,9 @@
 		<PropertyRow label="Color">
 			<ColorPicker
 				:modelValue="activeElement.borderColor || defaultBorderColor"
-				@update:modelValue="setBorderColor"
-				@colordown="beginBorderEdit"
-				@colorup="commitBorderEdit"
+				@update:modelValue="borderColor.set"
+				@colordown="borderColor.begin"
+				@colorup="borderColor.commit"
 			/>
 		</PropertyRow>
 		<NumberControl
@@ -23,9 +23,9 @@
 			:max="50"
 			:max-digits="3"
 			:step="0.5"
-			@update:modelValue="setBorderWidth"
-			@change-start="beginBorderEdit"
-			@change-end="commitBorderEdit"
+			@update:modelValue="borderWidth.set"
+			@change-start="borderWidth.begin"
+			@change-end="borderWidth.commit"
 		/>
 		<NumberControl
 			:modelValue="activeElement.borderRadius ?? 0"
@@ -52,10 +52,7 @@ import Section from '@/apps/slides/components/controls/Section.vue'
 import LineStyleSelect from '@/apps/slides/components/controls/LineStyleSelect.vue'
 
 import { activeElement } from '@/apps/slides/stores/element'
-import {
-	setElementProperties,
-	useElementProperty,
-} from '@/apps/slides/composables/editProperty'
+import { useElementProperty } from '@/apps/slides/composables/editProperty'
 import { defaultBorderColor, MAX_BORDER_RADIUS } from '@/apps/slides/utils/constants'
 
 const defaultBorderWidth = 1
@@ -73,56 +70,40 @@ const hasBorder = computed(() =>
 
 const displayStyle = computed(() => activeElement.value.borderStyle || 'none')
 
-const setBorderStyle = (style) => {
-	const width = Number(activeElement.value.borderWidth) || 0
-	setElementProperties([
-		{ property: 'borderStyle', oldValue: activeElement.value.borderStyle, newValue: style },
-		{
-			property: 'borderWidth',
-			oldValue: activeElement.value.borderWidth,
-			newValue: style === 'none' ? 0 : width || defaultBorderWidth,
-		},
-	])
-}
+const borderProperties = ['borderColor', 'borderWidth', 'borderStyle']
 
-const hasVisibleStyle = () =>
-	activeElement.value.borderStyle && activeElement.value.borderStyle !== 'none'
+const useBorderProperty = (property, setWithBorder) => {
+	const elementProperty = useElementProperty(property)
 
-let borderSnapshot = null
-
-const beginBorderEdit = () => {
-	const el = activeElement.value
-	borderSnapshot = {
-		borderColor: el.borderColor,
-		borderWidth: el.borderWidth,
-		borderStyle: el.borderStyle,
+	return {
+		...elementProperty,
+		set: (value) => elementProperty.setEach((el) => setWithBorder(el, value)),
+		begin: () => elementProperty.begin(borderProperties),
 	}
 }
 
-const setBorderColor = (value) => {
-	const el = activeElement.value
+const hasVisibleStyle = (el) => el.borderStyle && el.borderStyle !== 'none'
+
+const borderColor = useBorderProperty('borderColor', (el, value) => {
 	el.borderColor = value
-	if (!hasVisibleStyle()) el.borderStyle = 'solid'
+	if (!hasVisibleStyle(el)) el.borderStyle = 'solid'
 	if (!Number(el.borderWidth)) el.borderWidth = defaultBorderWidth
-}
+})
 
-const setBorderWidth = (value) => {
-	const el = activeElement.value
+const borderWidth = useBorderProperty('borderWidth', (el, value) => {
 	el.borderWidth = value
-	if (Number(value) && !hasVisibleStyle()) el.borderStyle = 'solid'
-}
+	if (Number(value) && !hasVisibleStyle(el)) el.borderStyle = 'solid'
+})
 
-const commitBorderEdit = () => {
-	if (!borderSnapshot) return
-	const el = activeElement.value
-	setElementProperties(
-		['borderColor', 'borderWidth', 'borderStyle'].map((property) => ({
-			property,
-			oldValue: borderSnapshot[property],
-			newValue: el[property],
-		})),
-	)
-	borderSnapshot = null
+const borderStyle = useBorderProperty('borderStyle', (el, style) => {
+	el.borderStyle = style
+	el.borderWidth = style === 'none' ? 0 : Number(el.borderWidth) || defaultBorderWidth
+})
+
+const setBorderStyle = (style) => {
+	borderStyle.begin()
+	borderStyle.set(style)
+	borderStyle.commit()
 }
 
 const borderRadius = useElementProperty('borderRadius')
